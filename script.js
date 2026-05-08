5d9779c434e5e9874da83e13f67fbe015a0860d9
edmarquesvicentesantos-ui <edmarquesvicentesantos@gmail.com>
2026-05-08 10:02:24 -0300

    Implement page opening and closing validation functions
    Added functions to handle page opening and closing validation.

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function validarFechamento() {
 document
   .querySelector(".fechar")
   .addEventListener("click", validarFechamento);
+function abrirPagina(pagina) {
+
+  window.location.href = pagina;
+
+}
+
+function validarFechamento() {
+
+  let saldo = 69;
+
+  if (saldo > 0) {
+
+    alert("Pagamento pendente nesta mesa!");
+
+  } else {
+
+    alert("Mesa fechada com sucesso!");
+
+  }
+
+}
+
+document
+  .querySelector(".fechar")
+  .addEventListener("click", validarFechamento);
