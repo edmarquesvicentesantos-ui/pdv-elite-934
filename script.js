252ae7b61eb4e1a601eae7e5cb5997f0d39909b5
edmarquesvicentesantos-ui <edmarquesvicentesantos@gmail.com>
2026-05-08 10:59:53 -0300

    Refactor validarFechamento and abrirPagina functions

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-function validarFechamento() {
-
-  let saldo = 69;
-
-  if (saldo > 0) {
-
-    alert("Pagamento pendente nesta mesa!");
-
-  } else {
-
-    alert("Mesa fechada com sucesso!");
-
-  }
-
-}
-
-document
-  .querySelector(".fechar")
-  .addEventListener("click", validarFechamento);
-function abrirPagina(pagina) {
+function abrirPagina(pagina){
 
   window.location.href = pagina;
 
 }
 
-function validarFechamento() {
+function validarFechamento(){
 
   let saldo = 69;
 
-  if (saldo > 0) {
+  if(saldo > 0){
 
     alert("Pagamento pendente nesta mesa!");
 
